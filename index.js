629f8a31ad872d5f42e9760fa554a726c5f82d22
Mat Wilson-Platform <wilsom47@mc-n357035.home>
2021-03-17 11:33:11 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const readline = require("readline-sync");
+var got = require("got");
+var location = [];
+var weather;
+var foundLocation = location;
+var prettyWeather = require("./prettyWeather.js");
+//import getWeather
+
+//Ask user for input, read user location inpit
+function displayMenuGetInput() {
+  console.log("Please enter your location:");
+  const userLocation = readline.prompt();
+  return userLocation;
+}
+
+//Converts response to json
+var options = {
+  responseType: "json",
+};
+
+//Retrieves all locations from API and outputs location information based on user input
+got(
+  "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=a0032f60-f816-4c7e-973a-b2adff757c84",
+  options
+).then((response) => {
+  location = response.body.Locations.Location;
+
+  
+  let userLocation = displayMenuGetInput();
+  let foundLocation = null;
+
+  for (let i = 0; i < location.length; i++) {
+    if (userLocation == location[i].name) {
+      console.log("Location found");
+      foundLocation = location[i];
+    }
+  }
+  if (foundLocation == null) {
+    console.log("Location not found");
+  } else {
+    console.log(foundLocation);
+  }
+
+  let weather = getWeather(foundLocation);
+});
+
+//Inputs location id into API URL and returns weather
+function getWeather(foundLocation) {
+  got(
+    `http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/${foundLocation.id}?res=3hourly&key=a0032f60-f816-4c7e-973a-b2adff757c84`,
+    options
+  ).then((response) => {
+    let weather = response.body.SiteRep.DV.Location.Period[0].Rep[0];
+    for (let key in weather) {
+      console.log(prettyWeather[key] + ' ' + weather[key]);
+    }
+  });
+}
---
+++ prettyWeather.js
@@ -0,0 +1,28 @@
+
+
+var prettyWeather = {
+    
+    F : 'Feels like',
+    
+    G: 'Wind gust',
+    
+    H : 'Relative humidity',
+    
+    T : 'Temperature',
+    
+    V : 'Visibility',
+    
+    D : 'Wind direction',
+    
+    S : 'Wind speed',
+    
+    U : 'Max UV',
+    
+    W : 'Weather type',
+    
+    Pp : 'Precipitation probability',
+    
+    $ : 'Minutes past midnight'
+};
+
+module.exports = prettyWeather; 
