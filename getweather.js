b79b4b417d517ee1cd98eaa7d5dd1f46a0e07474
Mat Wilson-Platform <wilsom47@mc-n357035.home>
2021-03-24 11:43:51 +0000

    User location returns weather

---
+++ getweather.js
@@ -0,0 +1,26 @@
+module.exports = prettyWeather; 
+
+var prettyWeather = {
+    
+    F : 'Feels like',
+    
+    G: 'Wind gust',
+    
+    H : 'Relative humidity',
+    
+    T : 'Temperature',
+    
+    V : 'Visibility',
+    
+    D : 'Wind direction',
+    
+    S : 'Wind speed',
+    
+    U : 'Max UV',
+    
+    W : 'Weather type',
+    
+    Pp : 'Precipitation probability',
+    
+    $ : 'Minutes past midnight'
+};
--- index.js
+++ index.js
@@ -19,15 +19,14 @@ var options = {
 };
 
 //Retrieves all locations from API and outputs location information based on user input
-got(
-  "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=a0032f60-f816-4c7e-973a-b2adff757c84",
-  options
-).then((response) => {
-  location = response.body.Locations.Location;
+let siteList = got("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=a0032f60-f816-4c7e-973a-b2adff757c84", options)
 
-  
+siteList.then(response => {
+  location = response.body.Locations.Location;
+  // console.log(location)
+
   let userLocation = displayMenuGetInput();
-  let foundLocation = null;
+  // let foundLocation = null;
 
   for (let i = 0; i < location.length; i++) {
     if (userLocation == location[i].name) {
@@ -35,6 +34,7 @@ got(
       foundLocation = location[i];
     }
   }
+
   if (foundLocation == null) {
     console.log("Location not found");
   } else {
@@ -42,7 +42,31 @@ got(
   }
 
   let weather = getWeather(foundLocation);
-});
+
+
+})
+
+// let weather1 = getWeather.userWeather
+// console.log(foundLocation.userWeather)
+function displayMenuforWeather() {
+  console.log("Please enter weather details:");
+  const userWeather = readline.prompt();
+  return userWeather;
+}
+
+
+function printWeather(weather) {
+  for (let key in weather) {
+    console.log(prettyWeather[key] + ' ' + weather[key]);
+  }
+  var realTime = (weather.$ / 60).toFixed(2)
+  console.log(realTime)
+
+  // let hours = realTime / 60;
+  // let minutes = (realTime % 60).toFixed(2);
+
+  // console.log(hours + ':' + minutes)
+}
 
 //Inputs location id into API URL and returns weather
 function getWeather(foundLocation) {
@@ -51,8 +75,10 @@ function getWeather(foundLocation) {
     options
   ).then((response) => {
     let weather = response.body.SiteRep.DV.Location.Period[0].Rep[0];
-    for (let key in weather) {
-      console.log(prettyWeather[key] + ' ' + weather[key]);
-    }
+    printWeather(weather)
+console.log(response.body.SiteRep.DV.Location)
+  }).catch(err => {
+    console.log('Ooops')
   });
+
 }
